802cb9e7054fd43a01d504466f988ea88408a163
revelation136 <revelation136@gmail.com>
2022-01-26 23:31:04 +0800

    DOM - adding src to script tag as attribute and make a separate file for JS

---
+++ counter.js
@@ -0,0 +1,20 @@
+// var: used to define a variable globally
+    // let: used to define a variable that is limited in scope to the current block such as a function or loop
+    // const: used to define a value that will not change
+    let counter = 0;
+
+    function count() {
+        counter++;
+        // counter = counter + 1; this is the root, counter++; is just shortcut
+        document.querySelector("h1").innerHTML = counter;
+        // select element h1 to display the current value of our variable counter
+    
+        if (counter % 10 === 0) {
+            alert(`Count is now ${counter}`);
+        }
+        // formatted string in python, using backticks instead of double or single quotation marks
+        // this is called templeted literal
+    }
+
+    document.querySelector('button').onclick = count;
+    // Find me the button of the page then onclick execute count function
