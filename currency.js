1de983d4954b9b28bd71cc23d32468f273d2d094
revelation136 <revelation136@gmail.com>
2022-02-06 20:00:37 +0800

    Currency - adding data value to body by using .innerHTML

--- currency.js
+++ currency.js
@@ -3,9 +3,16 @@
 fetch('https://api.exchangerate.host/latest')
 
 // Put response into json form
+// always take note that the argument of .then is always a function
+// response and data are just parameters  of two anonymous function. therefore they're functions
 .then(response => response.json())
 .then(data => {
     
-    // Log data to the console
     console.log(data);
+    // Get rate from data
+    const rate = data.rates.PHP;
+
+    // Display message on the screen
+    // toFixed(3) means 3 decimal values
+    document.querySelector('body').innerHTML = `1 USD is equal to ${rate.toFixed(3)}PHP.`
 });
